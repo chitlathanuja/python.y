3a2797d7321720413b0a0e2f11792da5ab31dde4
chitlathanuja <chitlathanuja06@gmail.com>
2021-05-18 21:28:39 +0530

    creating file

---
+++ learning.py
@@ -0,0 +1,104 @@
+#open close read write seek
+#def simple_read_few_bytes(filename):
+ #   fd = open(filename, "r")
+  #  data=fd.read(20)
+   # print("data :%s" % data)
+    #fd.close()
+
+#def simple_read_twice(file name)
+#   fd=open(file name"r")s
+#{
+
+
+ #   print("")
+#dict = ("name": "lekshana" age:"6" class:"first")
+      
+ #      (name : "lekshana")
+  #     (age : "6")
+   #    (class: "first")
+ #print("dict['name']",   dict['name'])
+#print ("dict['age']",    dict['age'])
+#print("")
+
+
+DICTIONARIES
+
+#print("")
+#dict = {'name': 'lekshana', 'age': '6', 'class': 'first'}
+       
+#print("dict['name']",       dict['name'])     
+#print("dict['age']",        dict['age'])
+#print("dict['class']",      dict['class'])
+
+print("")
+dict = {'state': 'Hyderabad', 'capital': 'hyderabad', 'hyderabad': '2014'}
+
+dict['hyderabad'] = 2013          
+
+print("dict['state']",         dict['state'])
+print("dict['capital]",        dict['capital'])
+print("dict['hyderabad']",   dict['hyderabad'])
+
+dict['City'] = 'H'
+
+print("dict['City']",  dict['City'])
+
+#print("")
+
+#dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First', 'phone': ['9902000000', '900000000']}
+
+#print("dict['Name']: ",  dict['Name'])
+
+#print("dict['Age']: ",   dict['Age'])
+
+#print("dict['Class']: ", dict['Class'])
+
+#print("dict['phone']: ", dict['phone'])
+
+#print("dict['phone']: ", dict['phone'][1])
+
+#print("dict['phone']: ", dict['phone'][0][0])
+
+
+LISTS
+
+
+
+list = ['p', 'r', 'o', 'b', 'e']
+
+print(list[0])
+
+#exit(1)
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+
+print (list[-1])
+print(list[-2])
+
+print (list [1:3])
+
+print (list [:-2])
+
+print (list [1:])
+
+print (list [:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
